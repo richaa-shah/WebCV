24cc90ba9180812f85ff31cba307f499d643a7ae
richaa-shah <16167921+richaa-shah@users.noreply.github.com>
2019-12-17 11:12:23 +0000

    Add header to page

---
+++ src/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+export const Header: React.FC = () => {
+  return (
+    <div className="Header">
+      <h1>Richa Shah</h1>
+      <br />
+      email: richashahmk@gmail.com
+      <br />
+      linkedIn: richashah12
+      <br />
+      mobile: 07772954104
+    </div>
+  );
+};
